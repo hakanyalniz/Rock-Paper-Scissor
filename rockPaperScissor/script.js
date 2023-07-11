69ff82801b78c4873d189801113fe4af82cec8e4
Hakan Yalniz <hakan_yalniz@live.com>
2023-07-11 16:35:13 +0300

    Added final counter.

--- rockPaperScissor/script.js
+++ rockPaperScissor/script.js
@@ -28,20 +28,26 @@ function whoWins(player, computer) {
     // Rock
     return "Draw!";
   } else if (player == "rock" && computer == "paper") {
+    computerCounter++;
     return "Player looses!";
   } else if (player == "rock" && computer == "scissor") {
+    playerCounter++;
     return "Player wins!";
   } else if (player == "paper" && computer == "rock") {
     // Paper
+    playerCounter++;
     return "Player wins!";
   } else if (player == "paper" && computer == "paper") {
     return "Draw!";
   } else if (player == "paper" && computer == "scissor") {
+    computerCounter++;
     return "Player looses!";
   } else if (player == "scissor" && computer == "rock") {
     //Scissor
+    computerCounter++;
     return "Player looses!";
   } else if (player == "scissor" && computer == "paper") {
+    playerCounter++;
     return "Player wins!";
   } else if (player == "scissor" && computer == "scissor") {
     return "Draw!";
@@ -52,8 +58,16 @@ function game() {
   console.log(whoWins(getPlayerChoice(), getComputerChoice()));
 }
 
+let computerCounter = 0;
+let playerCounter = 0;
+
 console.log("The game of Rock Paper Scissor is starting!");
 console.log("The player goes first!");
+
 for (let i = 0; i < 5; i++) {
   game();
 }
+
+console.log(
+  `Final results: Computer: ${computerCounter} Player: ${playerCounter}`
+);
