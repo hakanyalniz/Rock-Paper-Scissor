34823ed6c0feca46c9838fb6d2a257320df292b8
Hakan Yalniz <hakan_yalniz@live.com>
2023-08-02 16:23:09 +0300

    Added a UI and some new logics to work with UI

--- rockPaperScissor/script.js
+++ rockPaperScissor/script.js
@@ -14,53 +14,93 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getPlayerChoice() {
-  let playerChoice = prompt("Your turn!").toLowerCase();
+// function getPlayerChoice(choice = "none") {
+//   let playerChoice = choice;
 
-  console.log("The player picks: " + playerChoice);
-  return playerChoice;
-}
+//   console.log("The player picks: " + playerChoice);
+//   return playerChoice;
+// }
 
 function whoWins(player, computer) {
   player = player.toLowerCase();
+  console.log("The player picks: " + player);
+  console.log(computerCounter, playerCounter);
+
   computer = computer.toLowerCase();
   if (
     (player == "rock" && computer == "rock") ||
     (player == "paper" && computer == "paper") ||
     (player == "scissor" && computer == "scissor")
   ) {
+    changeScore("draw");
     return "Draw!";
   } else if (
     (player == "rock" && computer == "paper") ||
     (player == "scissor" && computer == "rock") ||
     (player == "paper" && computer == "scissor")
   ) {
-    computerCounter++;
+    changeScore("computer");
+    // computerCounter++;
     return "Player looses!";
   } else if (
     (player == "rock" && computer == "scissor") ||
     (player == "paper" && computer == "rock") ||
     (player == "scissor" && computer == "paper")
   ) {
-    playerCounter++;
+    changeScore("player");
+    // playerCounter++;
     return "Player wins!";
   }
 }
 
-function game() {
-  console.log(whoWins(getPlayerChoice(), getComputerChoice()));
+function changeScore(whoWon) {
+  if (whoWon == "computer") {
+    computerCounter++;
+    computerScore.innerHTML = computerCounter;
+  } else if (whoWon == "player") {
+    playerCounter++;
+    playerScore.innerHTML = playerCounter;
+  } else if (whoWon == "draw") {
+    drawCounter++;
+  } else {
+    throw new Error("Something went wrong in changeScore whoWon logic.");
+  }
+
+  // Computes and shows the last result
+  if (computerCounter == 5 || playerCounter == 5) {
+    console.log(
+      `Final results: Computer: ${computerCounter} Player: ${playerCounter}`
+    );
+    finalResults.innerHTML = `Final results: Computer: ${computerCounter} Player: ${playerCounter}`;
+  }
 }
+
+function game() {
+  rockButton.addEventListener("click", () =>
+    whoWins("rock", getComputerChoice())
+  );
+  paperButton.addEventListener("click", () =>
+    whoWins("paper", getComputerChoice())
+  );
+  scissorButton.addEventListener("click", () =>
+    whoWins("scissor", getComputerChoice())
+  );
+}
+
+// References the buttons and adds event listeners later on to get player input via UI
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorButton = document.getElementById("scissor");
+
+let computerScore = document.getElementById("computer-score");
+let playerScore = document.getElementById("player-score");
+let finalResults = document.getElementById("final-results");
 
 let computerCounter = 0;
 let playerCounter = 0;
+let drawCounter = 0;
 
 console.log("The game of Rock Paper Scissor is starting!");
 console.log("The player goes first!");
 
-for (let i = 0; i < 5; i++) {
-  game();
-}
-
-console.log(
-  `Final results: Computer: ${computerCounter} Player: ${playerCounter}`
-);
+game();
